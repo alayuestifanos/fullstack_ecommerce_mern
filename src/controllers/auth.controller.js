f58b1d8934e14d3c2f5f3019047e698d29d980a0
Alayu Estifanos <se.alayu.estifanos@gmail.com>
2024-03-13 12:55:14 +0300

    user sign up and cookie

---
+++ src/controllers/auth.controller.js
@@ -0,0 +1,31 @@
+import asyncHandler from "../services/asyncHandler.js";
+import CustomError from "../utils/customError.js";
+import User from "../models/user.schema.js";
+
+export const cookieOptions = {
+  expires: new Date(Date.now() + 3 * 24 * 60 * 60 * 1000),
+  httpOnly: true, //user can see the token but can not change the token we can see but we can't edit, must do from the server side
+};
+
+export const signUp = asyncHandler(async (req, res) => {
+  // get data from user
+  const { name, email, password } = req.body;
+  // validation
+  if (!name || !email || !password) {
+    // throw new Error("Request fill");
+    throw new CustomError("Request fill", 400);
+  }
+  const existingUser = await User.findOne({ email });
+  if (existingUser) {
+    throw new CustomError("User already exists", 400);
+  }
+  const user = await User.create({ name, email, password });
+  const token = user.getJwtToken();
+
+  //   store this token to the users cookie by default nodejs cant access it because cookie is browsers object, to access we need to  use cookie-parser
+  res.cookie("token", token, cookieOptions);
+  //  safety because for the creation operation select:false is not work in the schema it only works after that so flash out the password
+  user.password = undefined;
+  //   send back a response to the user
+  res.status(200).json({ success: true, token, user });
+});
