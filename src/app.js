9e6be1cb620f05fa71d37adc187ece333d634233
Alayu Estifanos <se.alayu.estifanos@gmail.com>
2025-01-01 18:18:03 +0300

    Mouting auth routes

--- src/app.js
+++ src/app.js
@@ -1,11 +1,14 @@
-import express from "express";
-import cookieParser from "cookie-parser";
-import cors from "cors";
+import express from 'express'
+import cookieParser from 'cookie-parser'
+import cors from 'cors'
 
-const app = express();
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(cors());
-app.use(cookieParser());
+const app = express()
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(cors())
+app.use(cookieParser())
 
-export default app;
+import authRoutes from './routes/auth.routes'
+
+app.use('api/v1/auth', authRoutes)
+export default app
