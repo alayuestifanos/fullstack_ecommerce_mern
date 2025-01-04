8410b725cb2a24a55db16d422e8423faabdd0c22
Alayu Estifanos <se.alayu.estifanos@gmail.com>
2025-01-04 19:20:08 +0300

    mouting auth routes

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,8 +1,16 @@
 import { Router } from 'express'
-import { signUp } from '../controllers/auth.controller'
+import {
+  signUp,
+  login,
+  logout,
+  getProfile,
+} from '../controllers/auth.controller'
 
 const router = Router()
 
 router.post('/signup', signUp)
+router.post('/login', login)
+router.post('/logout', logout)
+router.get('/me', getProfile)
 
 export default router
